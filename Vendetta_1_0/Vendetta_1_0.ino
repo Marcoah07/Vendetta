064d8888d0d956e8b0f88bcf6eca1119bb3076c0
Marcoah07 <marco.a.hurtado07@gmail.com>
2025-04-09 16:48:58 -0500

    Update variable names and add more variables
    Renamed variables to m closely mirror the their narrative contexts. Added more switch and servo objects for future use.

--- Vendetta_1_0/Vendetta_1_0.ino
+++ Vendetta_1_0/Vendetta_1_0.ino
@@ -3,33 +3,32 @@
 #include <Servo.h>//Servo library 
 
 //variables
-Servo servo1, servo2;//We can add more as needed
-int servPin1 = 9;//Pin for servo1
-int switch1 = 7;//Pin for switch 1 
-int ledPin1;//Pin for led1 
+Servo servoBailey, servoDance, servoTrain, servoFire;//We can add more servo objects as needed
+int switchDoor = 7, switchBailey, switchRoses, switchDance, switchTrain;//Pins for switches
+int ledDoor, ledBailey, ledRain;//Pin for led1 
 int frame = 0;//Track the active story frame
 
 void setup()
 {
 	Serial.begin(9600);//Talk to serial display 
-	servo1.attach(servPin1); //Attach servo to pin
-	pinMode(ledPin1, OUTPUT);
+	servoDoor.attach(9); //Attach servo to pin
+	pinMode(ledDoor, OUTPUT);
 }
 
 void loop()
 {
 	//Initialize positions and states
 	if (frame == 0){
-		servo1.write(0);
-		if (digitalRead(switch1) == HIGH){//Token is in starting place 
-			digitalWrite(ledPin1, HIGH);
+		servoDoor.write(0);
+		if (digitalRead(switchDoor) == HIGH){//Token is in starting place 
+			digitalWrite(ledDoor, HIGH);
 			frame++;}//Advance tracker
 	}//End of reset 
 	
 	//Larkhill 
 	if (frame == 1){
-		if (digitalRead(switch1) == LOW){//Once token is removed
-			digitalWrite(ledPin1, LOW);//Turn off led 
+		if (digitalRead(switchDoor) == LOW){//Once token is removed
+			digitalWrite(ledDoor, LOW);//Turn off led 
 			frame++;//Advance frame tracker 
 		}//End of if token removed 
 	}//End of Larkhill 
