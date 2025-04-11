910d07ddf01dfe8eafdc9bfd03b1d642ead47d86
Marcoah07 <marco.a.hurtado07@gmail.com>
2025-04-11 15:10:02 -0500

    Story logic updates
    Added scenes 3 through 5 and restart logic. Restructured story advancement to account for perpetual interactions. Servo and piezo code incomplete

--- Vendetta_1_0/Vendetta_1_0.ino
+++ Vendetta_1_0/Vendetta_1_0.ino
@@ -12,8 +12,12 @@ void setup()
 {
 	Serial.begin(9600);//Talk to serial display 
 	servoBailey.attach(9); //Attach servo to pin
+	servoDance.attach();
+	servoTrain.attach();
+	servoFire.attach();
 	pinMode(ledDoor, OUTPUT);
 	pinMode(ledBailey, OUTPUT);
+	pinMode(ledRain, OUTPUT);
 }
 
 void loop()
@@ -21,18 +25,21 @@ void loop()
 	//Initialize positions and states
 	if (frame == 0){
 		servoBailey.write(0);
+		servoDance.write(0);
+		servoTrain.write(0);
+		servoFire.write(0);
 		if (digitalRead(switchDoor) == HIGH){//Token is in starting place 
 			digitalWrite(ledDoor, HIGH);
-			frame++;}//Advance tracker
+			frame = 1;;}//Advance tracker
 	}//End of reset 
 	
 	//Larkhill 
 	if (frame == 1){
 		if (digitalRead(switchDoor) == LOW){//Once token is removed
 			digitalWrite(ledDoor, LOW);//Turn off led 
-			frame++;//Advance frame tracker 
+			frame = 2;;//Advance frame tracker 
 		}//End of if token removed 
-	}//End of Larkhill 
+	}//End of Larkhill
 	
 	//Old Bailey
 	if (frame == 2){
@@ -42,8 +49,40 @@ void loop()
 				delay(11);//To complete 90° in roughly 1 second 
 			}//End of sweep 
 			digitalWrite(ledBailey, HIGH);//Bailey explodes 
+			//piezo code 
 		}//End of token trigger 
+		if (digitalRead(switchRoses) == HIGH){//if roses are in place 
+		frame = 3;}
 	}//End of old bailey 
 	
-	//Add full story completion checking code here
-}//End of loop 
+	//Roses
+	if (frame == 3){
+		digitalWrite(ledRain, HIGH);
+		if (digitalRead(switchDance) == HIGH){//Advance to next frame 
+		frame = 4;}
+	}//End of Roses 
+	
+	//Dance 
+	if (frame == 4){
+		digitalWrite(ledRain, LOW);
+		//servo code 
+		//Piezo code
+		if (digitalRead(switchTrain) == HIGH){//Advance story upon next switch being closed 
+			frame = 5;}
+}//End of Dance 
+	
+	//Parliament
+	if (frame == 5){
+		servoTrain.write(180);
+		servoFire(.write(180);
+		//Piezo
+		if (servoFire.read() == 180){
+		frame == 6;}
+	}//End of parliament 
+	
+	
+	//Reset Checker
+	if (frame == 6 && digitalRead(switchBailey) == LOW && digitalRead(switchRoses) == LOW && digitalRead(switchDance) == LOW && digitalRead(switchTrain) == LOW){
+	frame = 0;}//Restart 
+	
+}//End of loop           
